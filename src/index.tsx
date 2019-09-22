13c58e68b9cfebdee79a78ffb3cc5a99ed3b6be4
Eating-Fish Flower <flower.eating.fish@gmail.com>
2019-09-22 21:47:26 +0800

    Add "Animate CSS" package

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 // index.tsx
 import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import "/node_modules/animate.css/animate.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
